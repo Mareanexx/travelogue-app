5dfd209ca1e7467fe310376b6bc33c30a1bc558c
mareanexx <mareanexx@mail.ru>
2025-05-13 12:51:01 +0300

    chore [BOTTOM_BAR]: added scale animation on nav bar items

--- app/src/main/java/ru/mareanexx/travelogue/presentation/components/BottomNavBar.kt
+++ app/src/main/java/ru/mareanexx/travelogue/presentation/components/BottomNavBar.kt
@@ -2,6 +2,9 @@ package ru.mareanexx.travelogue.presentation.components
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -14,10 +17,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -61,12 +66,25 @@ fun NavItem(
     selected: Boolean,
     navigateTo: () -> Unit
 ) {
+    val scale by animateFloatAsState(
+        targetValue = if (selected) 1.1f else 1f,
+        animationSpec = tween(
+            durationMillis = 200,
+            easing = FastOutSlowInEasing
+        ),
+        label = "NavItemScale"
+    )
+
     Column(
         modifier = Modifier
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = null
-            ) { navigateTo() },
+            ) { navigateTo() }
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+            },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
